b93bb7a01efbab7c9a2abbb5da8f0634741e1df0
Jayce Brubaker <brusclues1@gmail.com>
2023-03-01 21:16:41 -0500

    finished game

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,4 +1,6 @@
-currentPlayer= null;
+var currentPlayer= null;
+var playersChosen = null;
+var board=null;
 
 function getBoardValues() {
     var board = {
@@ -14,15 +16,24 @@ function checkForWin(board) {
     var boardRowOne = board[1];
     var boardRowTwo = board[2];
     var boardRowThree = board[3];
-    var boardColOne = [boardRowOne[0],boardRowTwo[0],boardRowThree[0]];
-    var boardColTwo = [boardRowOne[1],boardRowTwo[1],boardRowThree[1]];
-    var boardColThree = [boardRowOne[2],boardRowTwo[2],boardRowThree[2]]
     if (boardRowOne[0] == 'X' && boardRowOne[1] == 'X' && boardRowOne[2] == 'X') {
 	    return "X wins"
     } else if (boardRowTwo[0] == 'X' && boardRowTwo[1] == 'X' && boardRowTwo[2] == 'X') {
 	    return "X wins"
     } else if (boardRowThree[0] == 'X' && boardRowThree[1] == 'X' && boardRowThree[2] == 'X') {
 	    return "X wins"
+    } else if (board[1][0] == 'X' && board[2][0] == 'X' && board[3][0] == 'X') {
+        return "X wins"
+    } else if (board[1][1] == 'X' && board[2][1] == 'X' && board[3][1] == 'X') {
+        return "X wins"
+    } else if (board[1][2] == 'X' && board[2][2] == 'X' && board[3][2] == 'X') {
+        return "X wins"
+    } else if (board[1][0] == 'O' && board[2][0] == 'O' && board[3][0] == 'O') {
+        return "O wins"
+    } else if (board[1][1] == 'O' && board[2][1] == 'O' && board[3][1] == 'O') {
+        return "O wins"
+    } else if (board[1][2] == 'O' && board[2][2] == 'O' && board[3][2] == 'O') {
+        return "O wins"
     } else if  (boardRowOne[0] == 'O' && boardRowOne[1] == 'O' && boardRowOne[2] == 'O') {
 	    return "O wins"
     } else if (boardRowTwo[0] == 'O' && boardRowTwo[1] == 'O' && boardRowTwo[2] == 'O') {
@@ -30,28 +41,203 @@ function checkForWin(board) {
     } else if (boardRowThree[0] == 'O' && boardRowThree[1] == 'O' && boardRowThree[2] == 'O') {
 	    return "O wins"
     } else {
-        return "No one Wins"
+        return "No one wins"
     }
 
 };
 
 function main() {  
-    var board = getBoardValues();
+    board = getBoardValues();
     if (checkForWin(board) == "X wins") {
-        console.log("X wins") //Need code here to end the game if X wins
+        console.log("X wins");
+        alert("X wins, refresh your browser to play again");
     } else if (checkForWin(board) == "O wins") {
-        console.log("O wins") //Need code here to end the game if O wins
+        console.log("O wins");
+        alert("O wins, refresh your browser to play again.");
     };
 };
 
 function xClicked() {
     document.getElementById("x-or-y-container").innerHTML="";
-    currentPlayer = "X";
-    main();
+    currentPlayer = "X"
+    playersChosen = true;
 };
 
 function yClicked() {
     document.getElementById("x-or-y-container").innerHTML="";
-    currentPlayer = "Y";
-    main();
+    currentPlayer = "O";
+    playersChosen = true;
 };
+
+function topLeftClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[1][0]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("top-left-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+};
+
+function topMidClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[1][1]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("top-mid-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function topRightClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[1][2]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("top-right-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function midLeftClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[2][0]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("mid-left-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function midClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[2][1]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("mid-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function midRightClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[2][2]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("mid-right-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function bottomLeftClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[3][0]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("bottom-left-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function bottomMidClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[3][1]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("bottom-mid-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
+function bottomRightClicked() {
+    main()
+    var notCurrentPlayer = null;
+    var tile = board[3][2]
+    if (currentPlayer == "X") {
+        notCurrentPlayer = "O"
+    } else if (currentPlayer == "O") {
+        notCurrentPlayer = "X"
+    };
+    if (tile == "" && tile != currentPlayer && tile != notCurrentPlayer){
+        document.getElementById("bottom-right-value").innerHTML=currentPlayer;
+        main()
+        currentPlayer = notCurrentPlayer
+    } else if (playersChosen != true) {
+        alert("You must start the game first before you can play it.")
+    } else {
+        alert("You need to choose a square that isn't already filled.")
+    }
+}
